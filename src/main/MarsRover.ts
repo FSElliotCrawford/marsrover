b66777d74be69433170b06ebcbcd5f0cd17072b0
Elliot Crawford <elliot.crawford@cazoo.co.uk>
2021-02-19 09:33:49 +0000

    Add breaking test

---
+++ src/main/MarsRover.ts
@@ -0,0 +1,11 @@
+export class MarsRover {
+    private position = "0:0:N";
+
+    constructor() {
+
+    }
+
+    public getPosition() {
+        return this.position;
+    }
+}
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,7 +1,15 @@
-import {Example} from "../main/example";
+import {MarsRover} from "../main/MarsRover";
 
-describe('example test', () => {
-    it("should", () => {
+describe('On initial load', () => {
+    it("should place the rover at 0,0", () => {
+        const rover = new MarsRover();
+        expect(rover.getPosition()).toBe("0:0:N");
+    })
+})
 
+describe("When giving the rover instructions", () => {
+    it("should move to 0:1:N when sending M", () => {
+        const rover = new MarsRover();
+        expect(rover.getPosition()).toBe("0:1:N");
     })
 })
